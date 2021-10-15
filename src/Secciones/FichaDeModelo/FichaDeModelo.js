d149a8495848b7c958a19cc63472cd1ff4d8fe33
Mauro32dx <mauromam32dx@gmail.com>
2021-10-14 23:38:45 -0300

    Se agrega la seccion FichaDeModelo V0.1

--- src/Secciones/FichaDeModelo/FichaDeModelo.js
+++ src/Secciones/FichaDeModelo/FichaDeModelo.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import "./FichaDeModelo.css"
+import Imagen1 from "../../Imagenes/Imagen1.png";
+import Imagen2 from "../../Imagenes/Imagen2.jpg";
+import Imagen3 from "../../Imagenes/Imagen3.png";
+
 
 export default class FichaDeModelo extends React.Component {
     constructor(props){
@@ -11,14 +16,42 @@ export default class FichaDeModelo extends React.Component {
     render () {
         return (
             <div>
-                <h1>FICHA DE MODELO </h1>
-                <p> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500 </p>
-                <div>IMAGEN</div>
-                <div>CORRUSEL PARTE VEIHCULO</div>
-                <p> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500 </p>
-                <div>IMAGEN 2</div>
-                <p> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500 </p>
-                <div>IMAGEN 3</div>   
+                <section className="estilosSeccionEncabezado" > 
+                    <img src={Imagen1} alt="Foto"  className="ImagenVeiculo"/>
+                    <div className="ContenedorTextoSeccionEncabezado"> 
+                        <p  className="subtituloFicha"> <b>Hilux DX/SR</b></p>
+                        <h1 className="tituloFicha">Preparada para cualquier desafío </h1>
+                        <p className="descripcionFicha"> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500 </p>
+                    </div>
+              
+                </section>
+                <section style={{background:"rgb(119, 119, 134)"}} > 
+                    <br/>
+                    <br/>
+                    <br/>
+                    <p> CARRUSEL</p>
+                    <br/>
+                    <br/>
+                    <br/>
+                </section>
+                <section className="estilosSeccion" > 
+                    
+                    <div className="ContenedorTextoSeccion"> 
+                        
+                        <h4 className="tituloSeccion">Titulo de 20px</h4>
+                        <p className="descripcionSeccion"> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500 </p>
+                    </div>
+                    <img src={Imagen2} alt="Foto"  className="ImagenSeccion"/>
+                </section>
+                
+                <section className="estilosSeccion" style={{flexDirection:"row-reverse"}}> 
+                    <div className="ContenedorTextoSeccion"> 
+                        <h4 className="tituloSeccion">Titulo de 20px</h4>
+                        <p className="descripcionSeccion"> Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500 </p>
+                    </div>
+                    <img src={Imagen3} alt="Foto"  className="ImagenSeccion"/>              
+                </section>
+ 
             </div>
         )
     }
